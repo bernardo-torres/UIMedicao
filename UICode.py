58ab86ecdbfac44579c730f22578788a1daf6def
Bernardo <beftorres@hotmail.com>
2019-06-20 22:35:53 -0300

    Primeira UI

---
+++ UICode.py
@@ -0,0 +1,10 @@
+from UI import *
+import sys
+
+# Roda janela
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
